daa9f1d846926ac373a28ca636498da837e82a8d
Kolawole26 <kolenzy26@yahoo.com>
2022-01-22 09:59:00 +0100

    third commit

--- Components/Footer.js
+++ Components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-            <footer className="bg-secondary py-7 px-9 ">
+        <div className="bg-secondary py-7 px-9 ">
+            <footer >
                 <p className="md:text-right text-center md:text-xl">Eventful Moments by Kolawole</p>   
             </footer>
         </div>
