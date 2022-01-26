ff8f7c7bff5f8fafe84d8f0de0173d733f96f4d7
Kolawole26 <kolenzy26@yahoo.com>
2022-01-26 20:05:48 +0100

    fifth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#008289",
-        secondary: "#60CED4",
-        btn: "#5271FF",
-        borderColor: "#707070",
-        cyan: "#06C3B4",
-        danger: "#C34F06",
-        ash: "#B2B2B2",
-        yellowBg: "#FFF5A7",
+        'primary': '#008289',
+        'secondary': '#60CED4',
+        'btn': '#5271FF',
+        'borderColor': '#707070',
+        'cyan': '#06C3B4',
+        'danger': '#C34F06',
+        'ash': '#B2B2B2',
+        'yellowBg': '#FFF5A7',
       },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
