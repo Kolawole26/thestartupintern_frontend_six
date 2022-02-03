846dd5d785461ecb0ac64c7162b752f5afe0f926
Kolawole26 <kolenzy26@yahoo.com>
2022-02-03 15:09:45 +0100

    final commit

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,5 +1,6 @@
-import ReorderIcon from '@mui/icons-material/Reorder';
 import Link from 'next/link';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
 import Cookies from 'js-cookie'
 
 
@@ -20,9 +21,11 @@ function Navigation( {onClick, active, links} ) {
                    </div>
 
                     <div onClick={onClick} className={`md:hidden`}>
-                        <button>
-                            <ReorderIcon className="text-white stroke-current stroke-1"/>
-                        </button>
+                        { !active ? <button className='py-1 px-2 bg-black rounded'>
+                            <ReorderIcon className="text-white stroke-current stroke-1 "/>
+                            </button> : <button className='py-1 px-2 bg-black rounded'>
+                                <CancelPresentationIcon className="text-white stroke-current stroke-1 "/>
+                            </button> }
                     </div>
                    <nav className={`${!active && 'hidden'} absolute flex-col top-full w-full left-0 z-20 md:static md:w-auto  md:flex bg-primary`}>
                        <ul className="md:flex-row md:flex">
